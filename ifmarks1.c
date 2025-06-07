34852ac4e1cdf8ebe08f540c2b532d56a3ff5ce8
harsh8208 <piyadakr96@gmail.com>
2025-06-08 01:53:12 +0530

    Add files via upload

---
+++ ifmarks1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+
+  int marks;
+  printf("Enter marks = ");
+  scanf("%d", &marks);
+
+  if (marks>=40)
+  {
+    printf("PASS");
+  }
+  else
+  {
+    printf("FAIL");
+  }
+  
+
+  return 0;
+}
